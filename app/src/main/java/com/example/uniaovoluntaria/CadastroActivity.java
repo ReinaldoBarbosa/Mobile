6322d57541d5193bf0327449b35c9e157c72fb23
ReinaldoBarbosa <reinaldobarbosa034@gmail.com>
2024-06-27 10:59:46 -0300

    Correção em alguns Design

--- app/src/main/java/com/example/uniaovoluntaria/CadastroActivity.java
+++ app/src/main/java/com/example/uniaovoluntaria/CadastroActivity.java
@@ -33,21 +33,39 @@ public class CadastroActivity extends AppCompatActivity {
     private void validaDados() {
         String email = binding.editEmail.getText().toString().trim();
         String senha = binding.editSenha.getText().toString().trim();
+        String nome =  binding.editName.getText().toString().trim();
+        String dataNasc = binding.editDataNasc.toString().toString().trim();
 
-        if (!email.isEmpty()) {
-            if (!senha.isEmpty()) {
-                criarContaFireBase(email, senha);
-            } else {
-                Toast.makeText(this, "Informe uma Senha.", Toast.LENGTH_SHORT).show();
+        if (!nome.isEmpty()){
+            if (!email.isEmpty()) {
+                if (!dataNasc.isEmpty()){
+                    if (!senha.isEmpty()) {
+                        criarContaFireBase(email,senha);
+                    }
+                    else
+                    {
+                        Toast.makeText(this, "Informe uma Senha.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else
+                {
+                    Toast.makeText(this, "Informe sua Data de Nascimento.", Toast.LENGTH_SHORT).show();
+                }
             }
-        } else {
-            Toast.makeText(this, "Informe seu Email.", Toast.LENGTH_SHORT).show();
+            else
+            {
+                Toast.makeText(this, "Informe seu Email.", Toast.LENGTH_SHORT).show();
+            }
         }
+        else
+        {
+            Toast.makeText(this, "Informe seu Nome.", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
-    private void criarContaFireBase(String email, String senha){
-        mAuth.createUserWithEmailAndPassword(
-                email,senha
+    private void criarContaFireBase(String email,String senha){
+        mAuth.createUserWithEmailAndPassword(email,senha
         ).addOnCompleteListener(task -> {
             if (task.isSuccessful()){
                 finish();
