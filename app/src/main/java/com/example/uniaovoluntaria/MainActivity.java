00e91e738a3d1798909a8d126f4e937d61d23699
ReinaldoBarbosa <reinaldobarbosa034@gmail.com>
2024-08-13 08:17:42 -0300

    Criação do Navigation bar

--- app/src/main/java/com/example/uniaovoluntaria/MainActivity.java
+++ app/src/main/java/com/example/uniaovoluntaria/MainActivity.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import com.example.uniaovoluntaria.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +24,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
 
-        binding.btnDeslogar.setOnClickListener(view -> {
+        binding.frameLayout.setOnClickListener(view -> {
             FirebaseAuth.getInstance().signOut();
             finish();
             startActivity(new Intent(this, LoginActivity2.class));
         });
+
+
+        bottomNavigationView = findViewById(R.id.bottomNavView);
+        frameLayout = findViewById(R.id.frameLayout);
     }
 
 }
